0ddd36cb6210a00b1888cd480050805664651b72
Brezak <bezak.adamko@gmail.com>
2023-04-20 02:54:04 +0200

    Create class for fixed point arithmetic

---
+++ pacman/GameState/Pos.h
@@ -0,0 +1,66 @@
+#ifndef PACMAN_POS_H
+#define PACMAN_POS_H
+
+#include <climits>
+#include <compare>
+#include <cstdint>
+#include <numeric>
+
+namespace game
+{
+
+template<typename Store, unsigned int binary_decimals>
+    requires std::integral<Store>
+class FixedPointNum final
+{
+    static_assert(binary_decimals <= sizeof(Store) * CHAR_BIT, "Not enough space in Store to fit decimal digits");
+
+    Store store;
+
+public:
+    // Initializes a fixed point number with a zero
+    constexpr FixedPointNum() = default;
+    // Implicitly convert integers into FixedPointNumbers for ease of use
+    // NOLINTNEXTLINE(google-explicit-constructor)
+    constexpr FixedPointNum(Store value);
+
+    static constexpr FixedPointNum<Store, binary_decimals> from_bits(Store bits);
+
+    template<unsigned int other_decimals>
+    constexpr FixedPointNum<Store, binary_decimals> operator+(const FixedPointNum<Store, other_decimals> &rhs);
+    template<unsigned int other_decimals>
+    constexpr FixedPointNum<Store, binary_decimals> operator-(const FixedPointNum<Store, other_decimals> &rhs);
+    template<unsigned int other_decimals>
+    constexpr FixedPointNum<Store, binary_decimals> operator*(const FixedPointNum<Store, other_decimals> &rhs);
+    constexpr FixedPointNum<Store, binary_decimals> operator/(const Store &rhs);
+
+    template<unsigned int other_decimals>
+    constexpr FixedPointNum<Store, binary_decimals> &operator+=(const FixedPointNum<Store, other_decimals> &rhs);
+    template<unsigned int other_decimals>
+    constexpr FixedPointNum<Store, binary_decimals> &operator-=(const FixedPointNum<Store, other_decimals> &rhs);
+    template<unsigned int other_decimals>
+    constexpr FixedPointNum<Store, binary_decimals> &operator*=(const FixedPointNum<Store, other_decimals> &rhs);
+    constexpr FixedPointNum<Store, binary_decimals> &operator/=(const Store &other);
+    constexpr std::strong_ordering operator<=>(const FixedPointNum<Store, binary_decimals> &other) const = default;
+
+    constexpr FixedPointNum<Store, binary_decimals> integer() noexcept;
+    constexpr FixedPointNum<Store, binary_decimals> frac() noexcept;
+
+    constexpr Store ceil();
+    constexpr Store floor() noexcept;
+    constexpr Store round();
+
+
+private:
+    template<unsigned int other_decimals>
+    static constexpr FixedPointNum<Store, binary_decimals>
+    shift_towards_self(FixedPointNum<Store, other_decimals> value);
+};
+
+
+class Pos
+{
+};
+}    // namespace game
+
+#endif    // PACMAN_POS_H

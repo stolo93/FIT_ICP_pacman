0b673c06d689ea5073ed19d248260175edc9ffc1
Samuel <xstola03@stud.fit.vutbr.cz>
2023-04-25 20:30:47 +0200

    Added controller

---
+++ pacman/Controller/controller.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file controller.cpp
+ * @author Samuel Stolarik
+ * @date 2023-04-24
+ * Project - ICP - project PacMan
+ */
+
+#include "controller.h"
+#include <QObject>
+#include <QTimer>
+#include <iostream>
+
+namespace ctl {
+
+}

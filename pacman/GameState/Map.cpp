cc1b05ca9d037bab46ceaf471bd15127d0f2e1f0
Brezak <bezak.adamko@gmail.com>
2023-04-26 21:15:03 +0200

    Add map struct

--- pacman/GameState/Map.cpp
+++ pacman/GameState/Map.cpp
@@ -1,5 +1,27 @@
 #include "Map.h"
 
+#include <utility>
+
 namespace game
 {
+
+std::size_t Map::get_width() const
+{
+    return this->width;
+}
+
+std::size_t Map::get_height() const
+{
+    return this->height;
+}
+
+const QVector<QVector<MapElement>> &Map::get_map() const
+{
+    return this->map;
+}
+Map::Map(std::size_t width, std::size_t height, QVector<QVector<MapElement>> map)
+    : width(width), height(height), map(std::move(map))
+{
+}
+
 }    // namespace game

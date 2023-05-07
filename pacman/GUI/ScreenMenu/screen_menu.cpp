2b68a746026cc5e3b613a564fe05eab95506c021
Samuel <xstola03@stud.fit.vutbr.cz>
2023-05-07 21:34:58 +0200

    Added pacman headline

--- pacman/GUI/ScreenMenu/screen_menu.cpp
+++ pacman/GUI/ScreenMenu/screen_menu.cpp
@@ -10,10 +10,16 @@
 #include "widget_main_menu.h"
 
 #include <QGridLayout>
+#include <QLabel>
 
 ScreenMenu::ScreenMenu(QWidget *parent) : QWidget(parent)
 {
     auto menu_screen_layout = new QGridLayout(this);
+    auto pacman_text = new QLabel("PacMan", this);
+
+    QFont font = pacman_text->font();
+    font.setPointSize(36); // Set the font size to a large value
+    pacman_text->setFont(font);
 
     // Set stretches
     menu_screen_layout->setColumnStretch(0, 1);
@@ -27,7 +33,7 @@ ScreenMenu::ScreenMenu(QWidget *parent) : QWidget(parent)
     // Create main menu and insert it to the layout
     auto main_menu = new MainMenu(this);
     menu_screen_layout->addWidget(main_menu, 1, 1, Qt::AlignCenter);
-
+    menu_screen_layout->addWidget(pacman_text, 0, 1, Qt::AlignCenter);
     connect(main_menu, &MainMenu::clicked_new_game, this, &ScreenMenu::on_new_game_button);
     connect(main_menu, &MainMenu::clicked_load_game, this, &ScreenMenu::on_load_game_button);
     this->setLayout(menu_screen_layout);
